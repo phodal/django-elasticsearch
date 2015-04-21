93ff8021d5bc463dbec37485ead7b8d9c7ca65fe
Fengda HUANG <h@phodal.com>
2015-04-21 16:39:21 +0800

    add location to es

--- nx/search_indexes.py
+++ nx/search_indexes.py
@@ -13,14 +13,3 @@ class NoteIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Note
-
-    @staticmethod
-    def prepare_location(obj):
-        try:
-            return obj.location.point
-        except AttributeError:
-            return None
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(timestamp__lte=timezone.now())
--- nx/views.py
+++ nx/views.py
@@ -10,10 +10,7 @@ from haystack.utils.geo import Point
 def notes(request):
     form = NotesSearchForm(request.GET)
     notes = form.search()
-    print notes
-
     center = Point(-95.23947, 38.9637903)
-    max_dist = D(mi=1000)
-    sqs = SearchQuerySet().dwithin('location', center, max_dist)
+    sqs = SearchQuerySet().distance('location', center)
     print sqs
     return render_to_response('notes.html', {'notes': notes})
