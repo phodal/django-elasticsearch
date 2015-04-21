8b72e562e562a459469be0d96ea6f617e15ec5ce
Fengda HUANG <h@phodal.com>
2015-04-21 15:14:00 +0800

    Test on location search

--- nx/views.py
+++ nx/views.py
@@ -1,10 +1,19 @@
+from django.contrib.gis.measure import D
+
 from django.shortcuts import render_to_response
 
 from .forms import NotesSearchForm
+from haystack.query import SearchQuerySet
 from haystack.utils.geo import Point
 
 
 def notes(request):
     form = NotesSearchForm(request.GET)
     notes = form.search()
+    print notes
+
+    center = Point(-95.23947, 38.9637903)
+    max_dist = D(mi=1000)
+    sqs = SearchQuerySet().dwithin('location', center, max_dist)
+    print sqs
     return render_to_response('notes.html', {'notes': notes})
