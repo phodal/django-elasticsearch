b0e23e7d3c94d8af633e2d0027a442c107ec0a12
Fengda HUANG <h@phodal.com>
2015-04-21 09:18:43 +0800

    Add basic templates

--- settings.py
+++ settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "zh-cn"
+LANGUAGE_CODE = "en"
 
 # Supported languages
 LANGUAGES = (
