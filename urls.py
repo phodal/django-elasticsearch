4d0b655c2f67c9f47d5aeb216eff31c046793d95
Fengda HUANG <h@phodal.com>
2015-04-21 12:45:19 +0800

    try haystack

--- urls.py
+++ urls.py
@@ -69,6 +69,7 @@ urlpatterns += patterns('',
     # ``mezzanine.urls``, go right ahead and take the parts you want
     # from it, and use them directly below instead of using
     # ``mezzanine.urls``.
+    (r'^search/', include('haystack.urls')),
     ("^", include("mezzanine.urls")),
 
     # MOUNTING MEZZANINE UNDER A PREFIX
