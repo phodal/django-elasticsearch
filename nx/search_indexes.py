6ba247931142bc9ee8b57ed370de701e1d9844f2
Fengda HUANG <h@phodal.com>
2015-04-22 09:13:40 +0800

    fix issue on filed

--- nx/search_indexes.py
+++ nx/search_indexes.py
@@ -11,7 +11,7 @@ class NoteIndex(indexes.SearchIndex, indexes.Indexable):
     price = indexes.IntegerField(model_attr='price')
     phone_number = indexes.IntegerField(model_attr='phone_number')
     location = indexes.LocationField(model_attr='get_location')
-    location_info = indexes.LocationField(model_attr='get_location_info')
+    location_info = indexes.CharField(model_attr='get_location_info')
 
     def get_model(self):
         return Note
