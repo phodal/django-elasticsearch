7f2d5fef98ae024fc04bb04325da1285360074dd
Fengda HUANG <h@phodal.com>
2015-04-22 08:47:36 +0800

    update indexs

--- nx/search_indexes.py
+++ nx/search_indexes.py
@@ -7,6 +7,9 @@ class NoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     body = indexes.CharField(model_attr='body')
+    number = indexes.IntegerField(model_attr='number')
+    price = indexes.IntegerField(model_attr='price')
+    phone_number = indexes.IntegerField(model_attr='phone_number')
     location = indexes.LocationField(model_attr='get_location')
     location_info = indexes.LocationField(model_attr='get_location_info')
 
