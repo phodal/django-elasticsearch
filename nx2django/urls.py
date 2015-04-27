a9a666cf6acc5bbefdcfbc0bbfd3829581406f1f
Fengda HUANG <h@phodal.com>
2015-04-27 08:47:40 +0800

    fix api ui error

--- nx2django/urls.py
+++ nx2django/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nx2django.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    ("^api/", include("api.urls")),
     url(r'^admin/', include(admin.site.urls)),
 )
