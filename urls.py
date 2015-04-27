6983bf5fdec7d4343df7bf21c3643bb950fdf346
Fengda HUANG <h@phodal.com>
2015-04-27 08:36:29 +0800

    move create to admin page

--- urls.py
+++ urls.py
@@ -4,17 +4,11 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from mezzanine.core.views import direct_to_template
-from nx.views import NoteCreate
 
 admin.autodiscover()
 
 urlpatterns = i18n_patterns("",
     ("^admin/", include(admin.site.urls)),
-)
-
-urlpatterns += patterns('nx.views',
-    url("^view/$", "notes", name="note"),
-    url("^create/$",NoteCreate.as_view(success_url="/view/"), name="create"),
 )
 
 urlpatterns += patterns('',
