bd28c21dce06f15114144d2f71d6ca761246822c
Fengda HUANG <h@phodal.com>
2015-05-03 14:05:35 +0800

    remove unique of username

--- nx/models.py
+++ nx/models.py
@@ -12,12 +12,7 @@ from pygeocoder import Geocoder
 
 
 class Note(models.Model):
-    username = models.CharField("用户名", max_length=30, unique=True)
-        # help_text=_('Required. 30 characters or fewer. Letters, numbers and '
-        #             '@/./+/-/_ characters'),
-        # validators=[
-        #     validators.RegexValidator(re.compile('^[\w.@+-]+$'), _('Enter a valid username.'), 'invalid')
-        # ])
+    username = models.CharField("用户名", max_length=30)
     email = models.EmailField("邮箱", blank=True)
     phone_number = models.CharField("手机号", max_length=11, unique=True,
         validators=[
